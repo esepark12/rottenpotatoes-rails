c0a872d8e3d92e9f5472ce87e4193ca67139f881
EC2 Default User <ec2-user@ip-172-31-31-136.us-east-2.compute.internal>
2021-02-18 20:39:28 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,11 +13,13 @@ class MoviesController < ApplicationController
     ######added
     @all_ratings = Movie.all_ratings #
     @ratings_to_show = params[:ratings] || {}
+    @ratings_list = @ratings_to_show
     if @ratings_to_show == {}
-      @ratings_to_show = Hash[@all_ratings.map {|x| [x, 1]}] #assign any value
+      @ratings_list = Hash[@all_ratings.map {|x| [x, 1]}] #assign any value
     end
     #update movies filtered by ratings
-    @movies = Movie.with_ratings(@ratings_to_show.keys)
+    
+    @movies = Movie.with_ratings(@ratings_list.keys)
     
     ######
   end
