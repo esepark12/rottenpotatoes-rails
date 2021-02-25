8c35722cc605032ac217c9837ce6967cd6ebd756
EC2 Default User <ec2-user@ip-172-31-31-136.us-east-2.compute.internal>
2021-02-25 19:47:00 +0000

    part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,10 +14,12 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings #
     @ratings_to_show = params[:ratings] || {} 
     session[:ratings] = @ratings_to_show
+    @ratings_to_show = session[:ratings]
     ratings_list = @ratings_to_show
     if @ratings_to_show == {}
       ratings_list = Hash[@all_ratings.map {|x| [x, 1]}] #assign any value
     end
+    
     #update movies filtered by ratings
     
     @movies = Movie.with_ratings(ratings_list.keys)
@@ -25,7 +27,7 @@ class MoviesController < ApplicationController
     @clicked_header = params[:clicked_header] || "" #session[:clicked_header] || ""
     session[:clicked_header] = @clicked_header
     #sort movies in order
-    if @clicked_header == "title_header"
+    if session[:clicked_header]#@clicked_header == "title_header"
       @movies = @movies.order(:title)
     end
     if @clicked_header == "release_date_header"
